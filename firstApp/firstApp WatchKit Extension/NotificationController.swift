f38ee694b27db78dbc4be9af68b74e5e777f95d6
NishiNo <nishino@xiyedeMacBook-Pro.local>
2018-12-28 20:07:25 +0900

    fix

---
+++ firstApp/firstApp WatchKit Extension/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  firstApp WatchKit Extension
+//
+//  Created by 西野 on 2018/12/28.
+//  Copyright © 2018年 西野. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
---
+++ firstApp/firstAppTests/firstAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  firstAppTests.swift
+//  firstAppTests
+//
+//  Created by 西野 on 2018/12/28.
+//  Copyright © 2018年 西野. All rights reserved.
+//
+
+import XCTest
+@testable import firstApp
+
+class firstAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
